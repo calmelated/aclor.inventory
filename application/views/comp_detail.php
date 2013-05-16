060602f04f05f21c5c6da432a7bf5bf31e007463
Chad Hsu <calmelated@gmail.com>
2013-05-16 15:04:28 -0500

    centralize the form action for all detail pages

--- application/views/comp_detail.php
+++ application/views/comp_detail.php
@@ -67,37 +67,14 @@
         </div>
     </div>
 
-    <div class="form-actions">
-        <input type="hidden" name="add_next" value="0">
-
-        <?php if($this->session->userdata['user_auth'] > 0) { /* auth: operator, manager, admin  */ ?>
-            <?php if($act == "detail_add") { ?>
-                <button type="submit" class="btn btn-primary" onclick='javascript:$("input[name=add_next]").val(1);'>
-                    <i class="icon-ok icon-white"></i> Save &amp; Next
-                </button>
-                <button type="submit" class="btn btn-info">
-                    <i class="icon-ok icon-white"></i> Save
-                </button>
-                <button type="reset" class="btn btn-link">Cancel</button>
-            <?php } else if($act == "detail_edit") { ?>
-                <button type="submit" class="btn btn-info">
-                    <i class="icon-ok icon-white"></i> Save
-                </button>
-                <button type="reset" class="btn btn-link">Cancel</button>
-            <?php } ?>
-        <?php } else { ?>
-            <button type="button" class="btn btn-info" onclick="javascript:window.history.back();">
-                <i class="icon-share-alt icon-white"></i> Back
-            </button>
-        <?php } ?>
-
-        <?php if(($this->session->userdata['user_auth'] > 1) &&   /* auth:2 admin */
-                 ($act == "detail_edit")) { ?>
-            <a href="comp/remove/<?=$comp['id']; ?>" class="btn btn-danger pull-right">
-                <i class="icon-trash icon-white"></i> Delete
-            </a>
-        <?php } ?>
-    </div>
+    <?php
+        $data['act']    = $act;
+        $data['add']    = 'comp/add';
+        $data['remove'] = (isset($comp['id'] )) ? 'comp/remove/' . $comp['id'] : '';
+        $data['edit']   = (isset($comp['id'] )) ? 'comp/edit/'   . $comp['id'] : '';
+        $data['cid']    = (isset($comp['cid'])) ? $comp['cid']                 : -1;
+        $this->load->view('formact', $data);
+    ?>
 
 </form>
 
--- application/views/outodr_detail.php
+++ application/views/outodr_detail.php
@@ -100,48 +100,15 @@
         </div>
     </div>
 
-    <div class="form-actions">
-        <input type="hidden" name="add_next" value="0">
+    <?php
+        $data['act']    = $act;
+        $data['add']    = 'outodr/add';
+        $data['remove'] = (isset($outodr['id']))  ? 'outodr/id_del/'  . $outodr['id'] : '';
+        $data['edit']   = (isset($outodr['id']))  ? 'outodr/id_edit/' . $outodr['id'] : '';
+        $data['cid']    = (isset($outodr['cid'])) ? $outodr['cid']                    : -1;
+        $this->load->view('formact', $data);
+    ?>
 
-        <?php if($this->session->userdata['user_auth'] > 0) { /* auth: operator, manager, admin  */ ?>
-            <?php if($act == "detail_add") { ?>
-                <button type="submit" class="btn btn-primary" onclick='javascript:$("input[name=add_next]").val(1);'>
-                    <i class="icon-ok icon-white"></i> Save &amp; Next
-                </button>
-                <button type="submit" class="btn btn-info">
-                    <i class="icon-ok icon-white"></i> Save
-                </button>
-                <button type="reset" class="btn btn-link">Cancel</button>
-            <?php } else if($act == "detail_edit") { ?>
-                <button type="submit" class="btn btn-info">
-                    <i class="icon-ok icon-white"></i> Save
-                </button>
-                <button type="reset" class="btn btn-link">Cancel</button>
-            <?php } else { ?>
-                <a href="outodr/add" class="btn btn-primary">
-                    <i class="icon-ok icon-white"></i> Create Next
-                </a>
-            <?php } ?>
-
-            <?php if(($this->session->userdata['user_auth'] > 1) &&  /* auth:2 admin */
-                     ($act == "detail" || $act == "detail_edit")) { ?>
-                <a href="outodr/id_del/<?=$outodr['id']; ?>" class="btn btn-danger pull-right">
-                    <i class="icon-trash icon-white"></i> Delete
-                </a>
-            <?php } ?>
-        <?php } else { ?>
-            <button type="button" class="btn btn-info" onclick="javascript:window.history.back();">
-                <i class="icon-share-alt icon-white"></i> Back
-            </button>
-        <?php } ?>
-
-        <?php if(($this->session->userdata['user_auth'] > 1) &&  /* auth:2 admin */
-                 ($act == "detail")) { ?>
-            <a href="outodr/id_edit/<?=$outodr['id']; ?>" class="btn btn-info pull-right">
-                <i class="icon-edit icon-white"></i> Edit
-            </a>
-        <?php } ?>
-    </div>
 </form>
 
 <script src="script/scripts.js"></script>

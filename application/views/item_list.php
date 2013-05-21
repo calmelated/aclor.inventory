95c9f09ce9a60f9ed246e1d7b7750004b21c5b8c
Chad Hsu <calmelated@gmail.com>
2013-05-21 14:06:12 -0500

    1. modify config to remove index.php fro URL 2. modify item list by AJAX post

--- application/views/item_list.php
+++ application/views/item_list.php
@@ -1,29 +1,49 @@
+
 <div class="container">
     <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="list_table">
         <thead>
             <tr>
-                <th>Item #</th>
-                <th>Unit</th>
-                <th>Unit1</th>
-                <th>
+                <th class="span2">Item #</th>
+                <th class="span1">Unit</th>
+                <th class="span1">Unit1</th>
+                <th class="span1">
                     <a href="fileup/items" class="btn">Import</a>
                     <a href="item/add" class="btn btn-primary">New</a>
                 </th>
             </tr>
         </thead>
-        <tbody>
-            <?php foreach($items as $item) { ?>
-                <tr>
-                    <td class="span2"><?=$item['name'];?></td>
-                    <td class="span1"><?=$item['unit'];?></td>
-                    <td class="span1"><?=$item['unit1'];?></td>
-                    <td class="span1">
-                        <a href="item/edit/<?=$item['id']  ;?>" class="btn btn-info">Edit</a>
-                        <a href="item/remove/<?=$item['id'];?>" class="btn btn-danger">Delete</a>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
 
-</div> <!-- /container -->
+    </table>
+</div>
+
+<script>
+/* Table initialisation */
+$(document).ready(function() {
+    var oTable = $('#list_table').dataTable( {
+        "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+        "sPaginationType": "bootstrap",
+        "bProcessing": true,
+        "bServerSide": true,
+        "aaSorting": [[3, "desc"]],
+        "iDisplayLength": 10,
+        "aLengthMenu": [[10, 25, 50, 100, Math.pow(2,64)], [10, 25, 50, 100, 'All']],
+        "sAjaxSource": 'item/datatable',
+        "oLanguage": {
+            "sLengthMenu": "_MENU_ records per page"
+        },
+        //"fnInitComplete": function() {
+            //oTable.fnAdjustColumnSizing();
+        //},
+        'fnServerData': function(sSource, aoData, fnCallback) {
+            aoData.push({name: '<?=$this->security->get_csrf_token_name()?>', value : '<?=$this->security->get_csrf_hash()?>'});
+            $.ajax ({
+                'dataType': 'json',
+                'type'    : 'POST',
+                'url'     : sSource,
+                'data'    : aoData,
+                'success' : fnCallback
+            });
+        }
+    });
+});
+</script>
